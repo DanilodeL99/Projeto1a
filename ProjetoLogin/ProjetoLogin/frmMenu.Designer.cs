a1e7e3ab33414b58f29a76c0dd17f36a0c185168
Danilo <danilo.campos8@etec.sp.gov.br>
2025-08-20 17:35:06 -0300

    duplicando sair

--- ProjetoLogin/ProjetoLogin/frmMenu.Designer.cs
+++ ProjetoLogin/ProjetoLogin/frmMenu.Designer.cs
@@ -32,6 +32,7 @@
             this.btnCadastro = new System.Windows.Forms.Button();
             this.btnSair = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblMenu
@@ -74,11 +75,22 @@
             this.button1.Text = "CADASTRO";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(358, 267);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 33);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "SAIR";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // frmMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnSair);
             this.Controls.Add(this.btnCadastro);
@@ -96,5 +108,6 @@
         private System.Windows.Forms.Button btnCadastro;
         private System.Windows.Forms.Button btnSair;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
